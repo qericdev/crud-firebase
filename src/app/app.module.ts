43831bafc4436493d343b5a69c2d84dad70cc14b
Erick Rojas <qericrm@gmail.com>
2023-02-22 09:50:25 -0500

    Se agregó el diseño para el dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ListComponent } from './list/list.component';
+import { UserComponent } from './user/user.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashboardComponent,
-    ListComponent
+    ListComponent,
+    UserComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule
